96b3846ac90a21f1e06eecf89343d2b1f4f563d1
Mike Cooper <mythmon@gmail.com>
2013-11-07 17:10:09 -0800

    Fix distribution.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    requirements = [l.strip() for l in f]
+
 setup(
     name='chief-james',
     version='0.1',
+    modules=['james'],
     scripts=['james.py'],
     author='Mike Cooper',
     author_email='mcooper@mozilla.com',
-    url='https://github.com/mythmon/chief-james'
+    url='https://github.com/mythmon/chief-james',
+    install_requires=requirements,
+    package_data={'james': ['requirements.txt', 'README', 'LICENSE']}
 )
