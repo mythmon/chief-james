ce5bae9639ba71898823149e17912b0d0003c1b4
Mike Cooper <mythmon@gmail.com>
2013-11-07 16:50:25 -0800

    setup.py, .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='chief-james',
+    version='0.1',
+    scripts=['james.py'],
+    author='Mike Cooper',
+    author_email='mcooper@mozilla.com',
+    url='https://github.com/mythmon/chief-james'
+)
